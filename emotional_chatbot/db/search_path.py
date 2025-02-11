c106424de7486e9dfe37675a2ba6457bca755b10
Santiago Maldonado Rojas <santiagomaldonadorojas64@gmail.com>
2025-02-11 15:06:48 -0500

    Cambios de Modelo 2, buscador de Path automatico

---
+++ emotional_chatbot/db/search_path.py
@@ -0,0 +1,18 @@
+import os
+
+def buscar_archivo_por_nombre(nombre_archivo, unidad="C:/"):
+    rutas_encontradas = []
+    for raiz, _, archivos in os.walk(unidad):
+        if nombre_archivo in archivos:
+            rutas_encontradas.append(os.path.join(raiz, nombre_archivo))
+    
+    return rutas_encontradas
+
+nombre_a_buscar = "intents.json"
+rutas = buscar_archivo_por_nombre(nombre_a_buscar)
+
+if rutas:
+    for ruta in rutas:
+        print("Archivo encontrado en:", ruta)
+else:
+    print("Archivo no encontrado")
