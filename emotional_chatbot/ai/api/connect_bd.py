63f5d4adc781429b05012c96448fcc29e2a23ead
PattyDev <patriciaardilabaez@gmail.com>
2025-03-17 19:35:02 -0500

    Conexion de la base de datos con DigitalOcean

--- emotional_chatbot/ai/api/connect_bd.py
+++ emotional_chatbot/ai/api/connect_bd.py
@@ -1,61 +1,60 @@
+import os
 import mysql.connector
 from mysql.connector import Error
+from dotenv import load_dotenv #pip install python-dotenv
+load_dotenv() # Carga las variables de entorno del .env
 
 class Database:
-    def __init__(self, host="127.0.0.1", port=3306, user="root", password="1234", database="maiz"):
-        self.host = host
-        self.port = port
-        self.user = user
-        self.password = password
-        self.database = database
+    def _init_(self):
+        self.host = os.getenv("DB_HOST")
+        self.port = int(os.getenv("DB_PORT")) 
+        self.user = os.getenv("DB_USER")
+        self.password = os.getenv("DB_PASSWORD")
+        self.database = os.getenv("DB_NAME")
+        ssl_disabled=True  # Desactiva SSL temporalmente
         self.connection = None
         self.cursor = None
 
-    def connect(self): #Conexión a la base de datos
+    def connect(self):#Conexión a la base de datos
         try:
             self.connection = mysql.connector.connect(
                 host=self.host,
                 port=self.port,
                 user=self.user,
                 password=self.password,
-                database=self.database
+                database=self.database,
+                # ssl_ca=self.ssl_cert
             )
             if self.connection.is_connected():
+                print("Conexión exitosa a la base de datos en DigitalOcean")
                 self.cursor = self.connection.cursor()
-                print("Successfully connected to MySQL")
-                self.cursor.execute("SELECT DATABASE();")
-                print(f"Connected to the database: {self.cursor.fetchone()[0]}")
         except Error as e:
-            print(f"Error connecting to MySQL: {e}")
+            print(f"Error al conectar a MySQL: {e}")
 
     def disconnect(self): #Cierra la Conexión a la base de datos
         if self.connection and self.connection.is_connected():
             self.cursor.close()
             self.connection.close()
-            print("Connection closed")
+            print("Conexión cerrada")
 
     def execute_query(self, query, params=None): #Ejecuta instrucciones Select y devuelve
         try:
             self.cursor.execute(query, params or ())
             return self.cursor.fetchall()
         except Error as e:
-            print(f"Error executing query: {e}")
+            print(f"Error ejecutando consulta: {e}")
             return None
 
-    def execute_modification(self, query, values): #Maneja: insert, update y delete.
+    def execute_modification(self, query, values): #Maneja: insert, update y delete
         try:
             self.cursor.execute(query, values)
             self.connection.commit()
-            print("Operation completed successfully")
+            print("Operación completada")
         except Error as e:
-            print(f"Error modifying data: {e}")
+            print(f"Error modificando datos: {e}")
 
-if __name__ == "__main__":
-    db = Database()
-    db.connect()
-
+if _name_ == "_main_":
+    db = Database() #instancia
+    db.connect() #llama la instancia y la conecta con la bd
     db.execute_query("SELECT DATABASE();")
-
-    db.disconnect()
-
-
+    db.disconnect()#desconecta la bd
